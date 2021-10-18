39e48e95d77bc9675762e4cbe4a8d1d20f26976a
Jack <74297154+Sanware@users.noreply.github.com>
2021-10-18 11:23:43 +0100

    Add files via upload
    This installer provides the extra packages required for this fork. PyAudio, Pyttsx3 & SpeechRecognition.
    
    WARNING:
    This installer is primarily designed for use on Windows operating systems, so don't be surprised if it doesn't work properly on your Mac, Linux or any other OS.

---
+++ Installer.py
@@ -0,0 +1,8 @@
+# Installer for Speech Recognition ChatBot Fork
+
+import os
+
+os.system("python3 -m pip install pyttsx3")
+os.system("python3 -m pip install pipwin")
+os.system("python3 -m pipwin install pyaudio")
+os.system("python3 -m pip install speechrecognition")
